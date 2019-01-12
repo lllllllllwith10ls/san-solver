fa55fdc42693d7f973d7682faf6aee95cbd11c1b
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2019-01-12 10:53:54 -0600

    Update solve.js

--- solve.js
+++ solve.js
@@ -101,7 +101,7 @@ class Separator {
 				}
 				if(parentheses === 0) {
 					separator = false;
-					let sep = str.substring(marker+1,i-1);
+					let sep = str.substring(marker+1,i);
 					this.separators.push(new Separator(sep,this));
 					str = str.replace(" ",sep);
 				}
