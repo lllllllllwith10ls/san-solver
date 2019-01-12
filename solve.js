b4040c8784107dee21a857210ff06af236594150
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2019-01-12 10:50:13 -0600

    Update solve.js

--- solve.js
+++ solve.js
@@ -37,13 +37,13 @@ class SanArray {
 				if(parentheses === 0) {
 					separator = false;
 					let sep = str.substring(marker+1,i-1);
-					this.separators.push(new Separator(sep),this);
+					this.separators.push(new Separator(sep,this));
 					str = str.replace(sep," ");
 				}
 			} else {
 				if(str[i] === ",") {
 					str = str.replace(","," ");
-					this.separators.push(new Separator("1"),this);
+					this.separators.push(new Separator("1",this));
 				}
 				if(str[i-1] === "s" && str[i] === "(") {
 					subArray = true;
@@ -103,13 +103,13 @@ class Separator {
 				if(parentheses === 0) {
 					separator = false;
 					let sep = str.substring(marker+1,i-1);
-					this.separators.push(new Separator(sep),this);
+					this.separators.push(new Separator(sep,this));
 					str = str.replace(" ",sep);
 				}
 			} else {
 				if(str[i] === ",") {
 					str = str.replace(" ",str[i]);
-					this.separators.push(new Separator(1),this);
+					this.separators.push(new Separator("1",this));
 				}
 				if(str[i-1] === "s" && str[i] === "(") {
 					subArray = true;
