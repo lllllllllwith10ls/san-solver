69352b9faa4b1ef35825224be67a1b678c6e5ddc
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2019-01-13 09:42:44 -0600

    Update solve.js

--- solve.js
+++ solve.js
@@ -245,7 +245,7 @@ class Separator {
 			}
 		} else {
 			let ma = [0];
-			for(let i = 1; i < a.array.length; i++) {
+			for(let i = 1; i < a.separators.length; i++) {
 				if(Separator.level(a.separators[ma[0]],a.separators[i]) === a.array[i]) {
 					ma = [i];
 				} else if(Separator.level(a.separators[ma[0]],a.separators[i]) === "equal") {
@@ -253,7 +253,7 @@ class Separator {
 				}
 			}
 			let mb = [0];
-			for(let i = 1; i < b.array.length; i++) {
+			for(let i = 1; i < b.separators.length; i++) {
 				if(Separator.level(b.separators[mb[0]],b.separators[i]) === b.array[i]) {
 					mb = [i];
 				} else if(Separator.level(b.separators[mb[0]],b.separators[i]) === "equal") {
